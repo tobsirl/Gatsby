12fc35e6a10edd384839578781ca335a49a8ea7f
Paul Tobin <tobster111@gmail.com>
2020-08-29 16:39:42 +0100

    Installed SCSS plugin

--- greatshoes/src/pages/index.js
+++ greatshoes/src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
 
+import "../assets/scss/index.scss"
+
 export default function Home() {
-  return <div>Hello world!Test</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <header class="hero">
+        <div class="container">
+          <h1 class="hero__title">Test</h1>
+        </div>
+      </header>
+    </div>
+  )
 }
