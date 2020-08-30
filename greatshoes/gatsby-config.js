d14d0051c6fa3b7e63dcb5440de00c5d1db8eda8
Paul Tobin <tobster111@gmail.com>
2020-08-30 15:53:00 +0100

    Added an image to the page

--- greatshoes/gatsby-config.js
+++ greatshoes/gatsby-config.js
@@ -9,6 +9,15 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
         fonts: [
--- greatshoes/src/pages/index.js
+++ greatshoes/src/pages/index.js
@@ -1,8 +1,30 @@
 import React from "react"
-
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 import "../assets/scss/index.scss"
 
-export default function Home() {
+// export default ({ data }) => (
+//   <div>
+//     <h1>Hello gatsby-image</h1>
+//     <Img fixed={data.file.childImageSharp.fixed} />
+//   </div>
+// )
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "shoe-1.png" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fixed(width: 150, height: 150) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
+
+export default function Home({ data }) {
   return (
     <div>
       <header class="hero">
@@ -22,15 +44,27 @@ export default function Home() {
             <h2 class="section-title">Featured products</h2>
             <div class="split">
               <a href="" class="featured__item">
-                <img src="" alt="" className="featured__img" />
+                <Img
+                  fixed={data.file.childImageSharp.fixed}
+                  alt="Shoe 1"
+                  className="featured__img"
+                />
                 <p class="featured__details"></p>
               </a>
               <a href="" class="featured__item">
-                <img src="" alt="" className="featured__img" />
+                <img
+                  src="/src/assets/img/shoe-2.png"
+                  alt="Shoe 2"
+                  className="featured__img"
+                />
                 <p class="featured__details"></p>
               </a>
               <a href="" class="featured__item">
-                <img src="" alt="" className="featured__img" />
+                <img
+                  src="/src/assets/img/shoe-3.png"
+                  alt="Shoe 3"
+                  className="featured__img"
+                />
                 <p class="featured__details"></p>
               </a>
             </div>
@@ -38,7 +72,40 @@ export default function Home() {
         </section>
 
         <section class="our-products">
-          <div class="container"></div>
+          <div class="container">
+            <h2 class="section__title">Our products</h2>
+            <article class="product">
+              <img
+                src="/src/assets/img/shoe-4.png"
+                alt="Shoe 4"
+                class="product__image"
+                href="#"
+              />
+              <h3 class="product__title"></h3>
+              <p class="product__description"></p>
+              <a href="" class="btn"></a>
+            </article>
+            <article class="product">
+              <img
+                src="/src/assets/img/shoe-5.png"
+                alt="Shoe 5"
+                class="product__image"
+              />
+              <h3 class="product__title"></h3>
+              <p class="product__description"></p>
+              <a href="" class="btn"></a>
+            </article>
+            <article class="product">
+              <img
+                src="/src/assets/img/shoe-6.png"
+                alt="Shoe 6"
+                class="product__image"
+              />
+              <h3 class="product__title"></h3>
+              <p class="product__description"></p>
+              <a href="" class="btn"></a>
+            </article>
+          </div>
         </section>
       </main>
     </div>
