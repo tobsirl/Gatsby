494b46a5c50d8cca11f8ba1d3516a47f78494c22
Paul Tobin <tobster111@gmail.com>
2020-09-23 16:38:00 +0100

    Importing normalize.css to has a baseline css

--- master-gatsby/gatsby/src/components/Layout.js
+++ master-gatsby/gatsby/src/components/Layout.js
@@ -3,6 +3,8 @@ import React from 'react';
 import Nav from './Nav';
 import Footer from './Footer';
 
+import 'normalize.css';
+
 export default function Layout({ children }) {
   return (
     <div>
