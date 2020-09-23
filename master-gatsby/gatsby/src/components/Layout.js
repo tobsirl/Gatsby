f4346be192aa1981e1fc0513cfd7708c787a60f3
Paul Tobin <tobster111@gmail.com>
2020-09-23 16:59:03 +0100

    Adding Typography.js in Layout.js

--- master-gatsby/gatsby/src/components/Layout.js
+++ master-gatsby/gatsby/src/components/Layout.js
@@ -5,11 +5,13 @@ import Footer from './Footer';
 
 import 'normalize.css';
 import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 
 export default function Layout({ children }) {
   return (
     <div>
-    <GlobalStyles />
+      <GlobalStyles />
+      <Typography />
       <Nav />
       {children}
       <Footer />
