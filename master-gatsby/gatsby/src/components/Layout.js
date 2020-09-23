cf80518fe2fcc4302705e7f1a29499488eb68fce
Paul Tobin <tobster111@gmail.com>
2020-09-23 16:53:30 +0100

    Adding GlobalStyles.js in Layout.js

--- master-gatsby/gatsby/src/components/Layout.js
+++ master-gatsby/gatsby/src/components/Layout.js
@@ -4,10 +4,12 @@ import Nav from './Nav';
 import Footer from './Footer';
 
 import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
 
 export default function Layout({ children }) {
   return (
     <div>
+    <GlobalStyles />
       <Nav />
       {children}
       <Footer />
