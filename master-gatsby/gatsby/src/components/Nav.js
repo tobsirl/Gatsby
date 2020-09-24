0faf82d40b2ca134fb46e3c42dc507411c97ed43
Paul Tobin <tobster111@gmail.com>
2020-09-24 15:55:06 +0100

    Adding styling to the ul

--- master-gatsby/gatsby/src/components/Nav.js
+++ master-gatsby/gatsby/src/components/Nav.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
 
+const NavStyles = styled.nav`
+  margin-bottom: 3rem;
+  ul {
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    list-style: none;
+
+    display: grid;
+    grid-template-columns: 1fr 1fr auto 1fr 1fr;
+    grid-gap: 2rem;
+  }
+`;
 export default function Nav() {
   return (
-    <nav>
+    <NavStyles>
       <ul>
         <li>
           <Link to="/">Hot Now</Link>
@@ -21,6 +35,6 @@ export default function Nav() {
           <Link to="/orders">Orders</Link>
         </li>
       </ul>
-    </nav>
+    </NavStyles>
   );
 }
