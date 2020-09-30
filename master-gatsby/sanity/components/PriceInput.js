e9ec414f6f096461a66b4d8a3aba8a462a7cb565
Paul Tobin <tobster111@gmail.com>
2020-09-30 20:06:30 +0100

    Formatting the priceInput using Intl.NumberFormat

--- master-gatsby/sanity/components/PriceInput.js
+++ master-gatsby/sanity/components/PriceInput.js
@@ -2,22 +2,31 @@ import React from 'react';
 import PatchEvent, { set, unset } from 'part:@sanity/form-builder/patch-event';
 
 function createPatchFrom(value) {
-  PatchEvent.from(value === '' ? unset() : set(Number(value)));
+  return PatchEvent.from(value === '' ? unset() : set(Number(value)));
 }
+
+const formatMoney = Intl.NumberFormat('en-GB', {
+  style: 'currency',
+  currency: 'EUR',
+}).format;
 
 export default function PriceInput({ type, value, onChange, inputComponent }) {
   return (
     <div>
-      <h2>{type.title}</h2>
+      <h2>
+        {type.title} - {value ? formatMoney(value) : ''}
+      </h2>
       <p>{type.description}</p>
       <input
         type={type.name}
         value={value}
-        onChange={(event) => createPatchFrom(event.target.value)}
+        onChange={(event) => onChange(createPatchFrom(event.target.value))}
         ref={inputComponent}
       />
     </div>
   );
 }
 
-
+PriceInput.focus = function () {
+  this._inputElement.focus();
+};
