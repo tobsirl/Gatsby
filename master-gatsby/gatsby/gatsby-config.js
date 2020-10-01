edf6e7ed8033045e802ab22ac4c41c814607c6d7
Paul Tobin <tobster111@gmail.com>
2020-10-01 19:09:21 +0100

    Setup the sanity plugin, using dotenv to store the sanity token

--- master-gatsby/gatsby/gatsby-config.js
+++ master-gatsby/gatsby/gatsby-config.js
@@ -1,7 +1,24 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
 export default {
   siteMetadata: {
     title: `Slick's Slices`,
     siteUrl: `https://www.gatsbyjs.com`,
     description: `The best place place in the world`,
   },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      // this is the name of the plugin you are adding
+      resolve: `gatsby-source-sanity`,
+      options: {
+        projectId: `35ani96r`,
+        data: `production`,
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
 };
