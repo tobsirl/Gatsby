a6dbc4fc6bf5f67815e2020cad291f3c8ca0f3a3
Paul Tobin <tobster111@gmail.com>
2020-10-04 20:19:03 +0100

    Single pizza component

--- master-gatsby/gatsby/src/components/PizzaList.js
+++ master-gatsby/gatsby/src/components/PizzaList.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
+function SinglePizza({ pizza }) {
+  console.log(pizza);
+  return (
+    <>
+      <h3>{pizza.name}</h3>
+      <p>{pizza.toppings[0].name}</p>
+    </>
+  );
+}
+
 export default function PizzaList({ pizzas }) {
-  console.log(pizzas);
   return (
     <>
       <p>List of pizzas</p>
-      {pizzas.map(({ id, name }) => (
-        <p key={id}>{name}</p>
+      {pizzas.map((pizza) => (
+        <SinglePizza key={pizza.id} pizza={pizza} />
       ))}
     </>
   );
