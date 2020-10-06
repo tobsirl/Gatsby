0e54ca4641e4aa2275f3d1f5854f26a7efbb22a8
Paul Tobin <tobster111@gmail.com>
2020-10-06 14:54:12 +0100

    GraphQL query for getting the toppings

--- master-gatsby/gatsby/src/components/ToppingsFilter.js
+++ master-gatsby/gatsby/src/components/ToppingsFilter.js
@@ -1,6 +1,23 @@
+import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 
 export default function ToppingsFilter() {
+  // Get a list of all the toppings
+  const toppings = useStaticQuery(graphql`
+    query {
+      toppings: allSanityTopping {
+        nodes {
+          name
+          id
+          vegetarian
+        }
+      }
+    }
+  `);
+  // Get a list of all Pizzas with their toppings
+  // Count how many pizzas are in each topping
+  // Loop over the list of toppings and display the topping and the count of pizzas in that topping
+  // Link it up ... .... ....
   return (
     <div>
       <p>Toppings Filter</p>
