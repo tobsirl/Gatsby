a55a769df03604fe70bc330e5ebb1f918fc39435
Paul Tobin <tobster111@gmail.com>
2020-10-06 15:47:28 +0100

    Counting the number of toppings on the pizzas

--- master-gatsby/gatsby/src/components/ToppingsFilter.js
+++ master-gatsby/gatsby/src/components/ToppingsFilter.js
@@ -1,9 +1,40 @@
 import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 
+function countPizzasInToppings(pizzas) {
+  // return the pizzas
+  // return pizzas
+  console.log(pizzas);
+  const counts = pizzas
+    .map((pizza) => pizza.toppings)
+    .flat()
+    .reduce((acc, topping) => {
+      // check if this is an existing topping
+      const existingTopping = acc[topping.id];
+      if (existingTopping) {
+        // if it is, increment by 1
+        existingTopping.count += 1;
+      } else {
+        // otherwise create a new entry in our acc and set it to one
+        acc[topping.id] = {
+          id: topping.id,
+          name: topping.name,
+          count: 1,
+        };
+      }
+      return acc;
+    }, {});
+
+  // sort based on their count
+  const sortedToppings = Object.values(counts).sort(
+    (a, b) => b.count - a.count
+  );
+  return sortedToppings;
+}
 export default function ToppingsFilter() {
   // Get a list of all the toppings
-  const toppings = useStaticQuery(graphql`
+  // Get a list of all Pizzas with their toppings
+  const { toppings, pizzas } = useStaticQuery(graphql`
     query {
       toppings: allSanityTopping {
         nodes {
@@ -12,10 +43,23 @@ export default function ToppingsFilter() {
           vegetarian
         }
       }
+      pizzas: allSanityPizza {
+        nodes {
+          id
+          name
+          toppings {
+            name
+            id
+          }
+        }
+      }
     }
   `);
-  // Get a list of all Pizzas with their toppings
+  console.clear();
+
   // Count how many pizzas are in each topping
+  const toppingsWithCounts = countPizzasInToppings(pizzas.nodes);
+  console.log(toppingsWithCounts);
   // Loop over the list of toppings and display the topping and the count of pizzas in that topping
   // Link it up ... .... ....
   return (
