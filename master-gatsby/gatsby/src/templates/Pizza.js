7fb0020601ca9f23f9c7ac8f3caf213aed163235
Paul Tobin <tobster111@gmail.com>
2020-10-07 16:35:20 +0100

    GraphQL query to bring in the data into SinglePizzaPage template

--- master-gatsby/gatsby/src/templates/Pizza.js
+++ master-gatsby/gatsby/src/templates/Pizza.js
@@ -1,5 +1,33 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 
-export default function SinglePizzaPage() {
-  return <p>Single Pizza</p>;
+export default function SinglePizzaPage({ data }) {
+  console.log(data);
+  return (
+    <>
+      <p>Single Page</p>
+    </>
+  );
 }
+
+// This need to be dynamic based on the slug passed in via context in gatsby-node.js
+export const query = graphql`
+  query($slug: String!) {
+    pizza: sanityPizza(slug: { current: { eq: $slug } }) {
+      id
+      name
+      image {
+        asset {
+          fluid(maxWidth: 800) {
+            ...GatsbySanityImageFluid
+          }
+        }
+      }
+      toppings {
+        id
+        name
+        vegetarian
+      }
+    }
+  }
+`;
