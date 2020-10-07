c1890c9053a4018f9a99939e430ce096f20722be
Paul Tobin <tobster111@gmail.com>
2020-10-07 15:41:45 +0100

    Using a graphql query to get the data into createPages

--- master-gatsby/gatsby/gatsby-node.js
+++ master-gatsby/gatsby/gatsby-node.js
@@ -1,11 +1,29 @@
-function turnPizzasIntoPages(params)
-// 1. Get a template for this page
-// 2. Query all pizzas
-// 3. Loop over each pizza and create a page for that pizza
+import path from 'path';
+
+async function turnPizzasIntoPages({ graphql, actions }) {
+  // 1. Get a template for this page
+  const pizzaTemplate = path.resolve('./src/templates/Pizza.js');
+  // 2. Query all pizzas
+  const { data } = await graphql(`
+    query {
+      pizzas: allSanityPizza {
+        nodes {
+          name
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+  console.log(data);
+  // 3. Loop over each pizza and create a page for that pizza
+}
 
 export async function createPages(params) {
   // Create pages dynamically
   // 1. Pizzas
+  await turnPizzasIntoPages(params);
   // 2. Toppings
   // 3. Slicemasters
 }
