62d41839c5258746a1d826b320fbb2aa91515bf3
Paul Tobin <tobster111@gmail.com>
2020-10-08 15:45:27 +0100

    Styling on the selected toppings

--- master-gatsby/gatsby/src/components/ToppingsFilter.js
+++ master-gatsby/gatsby/src/components/ToppingsFilter.js
@@ -19,7 +19,7 @@ const ToppingStyles = styled.div`
       background: white;
       padding: 2px 5px;
     }
-    .active {
+    &.active {
       background: var(--yellow);
     }
   }
@@ -55,7 +55,7 @@ function countPizzasInToppings(pizzas) {
   );
   return sortedToppings;
 }
-export default function ToppingsFilter() {
+export default function ToppingsFilter({ activeTopping }) {
   // Get a list of all the toppings
   // Get a list of all Pizzas with their toppings
   const { pizzas } = useStaticQuery(graphql`
@@ -81,8 +81,16 @@ export default function ToppingsFilter() {
   // Link it up ... .... ....
   return (
     <ToppingStyles>
+      <Link to="/pizzas">
+        <span className="name">All</span>
+        <span className="count">{pizzas.nodes.length}</span>
+      </Link>
       {toppingsWithCounts.map((topping) => (
-        <Link key={topping.id} to={`/topping/${topping.name}`}>
+        <Link
+          key={topping.id}
+          className={topping.name === activeTopping ? 'active' : ''}
+          to={`/topping/${topping.name}`}
+        >
           <span className="name">{topping.name}</span>
           <span className="count">{topping.count}</span>
         </Link>
