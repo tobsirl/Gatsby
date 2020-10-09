4abd9afc9ef686b87bf8c11e37c0a51f47d83754
Paul Tobin <tobster111@gmail.com>
2020-10-09 17:10:09 +0100

    query the data and pull it into the beers page

--- master-gatsby/gatsby/src/pages/beers.js
+++ master-gatsby/gatsby/src/pages/beers.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function FourOhFourPage() {
+export default function FourOhFourPage({ data }) {
+  const beers = data.beers.nodes;
+  console.log(beers);
   return (
     <>
       <p>Hey - This is the Beer page</p>
+      <ul>
+        {beers.map((beer) => (
+          <li key={beer.id}>{beer.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
+
+export const query = graphql`
+  query BeersQuery {
+    beers: allBeer {
+      nodes {
+        name
+        id
+        price
+        rating {
+          reviews
+          average
+        }
+      }
+    }
+  }
+`;
