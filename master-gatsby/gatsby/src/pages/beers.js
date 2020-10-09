afe65087890a8ad31dc6cff04dba3f90f39dfcfe
Paul Tobin <tobster111@gmail.com>
2020-10-09 17:59:19 +0100

    Styling of the beers page

--- master-gatsby/gatsby/src/pages/beers.js
+++ master-gatsby/gatsby/src/pages/beers.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import styled from 'styled-components';
 
+const BeerGridStyles = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+`;
+
+const SingleBeerStyles = styled.div`
+  border: 1px solid var(--grey);
+  padding: 2rem;
+  text-align: center;
+  img {
+    width: 100%;
+    height: 200px;
+    object-fit: contain;
+    display: grid;
+    align-items: center;
+    font-size: 10px;
+  }
+`;
 export default function BeersPage({ data }) {
   const beers = data.beers.nodes;
-  // console.table(beers);
+
   return (
     <>
       <h2 className="center">
         We have {beers.length} Beers Available. Dine in Only!
       </h2>
-      <div>
+      <BeerGridStyles>
         {beers.map((beer) => {
           const rating = Math.round(beer.rating.average);
           return (
-            <div key={beer.id}>
+            <SingleBeerStyles key={beer.id}>
               <img src={beer.image} alt={beer.name} />
               <h3>{beer.name}</h3>
               {beer.price}
@@ -22,12 +42,14 @@ export default function BeersPage({ data }) {
                 <span style={{ filter: `grayscale(100%)` }}>
                   {'⭐'.repeat(5 - rating)}
                 </span>
-                <span>{beer.rating.reviews}</span>
+                <span style={{ marginLeft: `10px` }}>
+                  {beer.rating.reviews}
+                </span>
               </p>
-            </div>
+            </SingleBeerStyles>
           );
         })}
-      </div>
+      </BeerGridStyles>
     </>
   );
 }
