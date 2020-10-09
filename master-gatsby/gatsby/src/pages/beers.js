2bf94b62d56c13b48d2c8b791e4b7ccb444d8d46
Paul Tobin <tobster111@gmail.com>
2020-10-09 17:31:39 +0100

    Constructing the BeersPage

--- master-gatsby/gatsby/src/pages/beers.js
+++ master-gatsby/gatsby/src/pages/beers.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
-export default function FourOhFourPage({ data }) {
+export default function BeersPage({ data }) {
   const beers = data.beers.nodes;
-  console.log(beers);
+  // console.table(beers);
   return (
     <>
-      <p>Hey - This is the Beer page</p>
-      <ul>
-        {beers.map((beer) => (
-          <li key={beer.id}>{beer.name}</li>
-        ))}
-      </ul>
+      <h2 className="center">
+        We have {beers.length} Beers Available. Dine in Only!
+      </h2>
+      <div>
+        {beers.map((beer) => {
+          const rating = Math.round(beer.rating.average);
+          return (
+            <div key={beer.id}>
+              <img src={beer.image} alt={beer.name} />
+              <h3>{beer.name}</h3>
+              {beer.price}
+              <p title={`${rating} out of 5 stars`}>
+                {'⭐'.repeat(rating)}
+                <span style={{ filter: `grayscale(100%)` }}>
+                  {'⭐'.repeat(5 - rating)}
+                </span>
+                <span>{beer.rating.reviews}</span>
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
@@ -27,6 +43,7 @@ export const query = graphql`
           reviews
           average
         }
+        image
       }
     }
   }
