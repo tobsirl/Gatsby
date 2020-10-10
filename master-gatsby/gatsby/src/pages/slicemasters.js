3758dfcf5db865437dc00b135ea732c846fa5d8c
Paul Tobin <tobster111@gmail.com>
2020-10-10 15:29:55 +0100

    Pulling in the people using the allSanityPerson

--- master-gatsby/gatsby/src/pages/slicemasters.js
+++ master-gatsby/gatsby/src/pages/slicemasters.js
@@ -1,9 +1,43 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 
-export default function SlicemastersPage() {
+export default function SlicemastersPage({ data }) {
+  console.log();
+  const slicemasters = data.slicemasters.nodes;
+  console.log(slicemasters);
+
   return (
     <>
-      <p>Hey, this is slice masters page</p>
+      <p>
+        Hey, this is slice masters page, we have {data.slicemasters.totalCount}{' '}
+        slicemasters
+      </p>
+      {slicemasters.map((master) => (
+        <p key={master.id}>{master.name}</p>
+      ))}
     </>
   );
 }
+
+export const query = graphql`
+  query SliceMastersQuery {
+    slicemasters: allSanityPerson {
+      totalCount
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        description
+        image {
+          asset {
+            fluid(maxWidth: 410) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
