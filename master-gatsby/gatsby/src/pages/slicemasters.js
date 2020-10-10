ec5928382c37c1362c2d24240115ab04590bed0d
Paul Tobin <tobster111@gmail.com>
2020-10-10 16:18:17 +0100

    Styling of each individual person

--- master-gatsby/gatsby/src/pages/slicemasters.js
+++ master-gatsby/gatsby/src/pages/slicemasters.js
@@ -9,23 +9,49 @@ const SlicemasterGrid = styled.div`
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
 `;
 
+const SlicemasterStyles = styled.div`
+  a {
+    text-decoration: none;
+  }
+  .gatsby-image-wrapper {
+    height: 400px;
+  }
+  h2 {
+    transform: rotate(-2deg);
+    text-align: center;
+    font-size: 4rem;
+    margin-bottom: -2rem;
+    position: relative;
+    z-index: 2;
+  }
+  .description {
+    background: var(--yellow);
+    padding: 1rem;
+    margin: 2rem;
+    margin-top: -6rem;
+    z-index: 2;
+    position: relative;
+    transform: rotate(1deg);
+    text-align: center;
+  }
+`;
+
 export default function SlicemastersPage({ data }) {
   const slicemasters = data.slicemasters.nodes;
-  console.log(slicemasters);
 
   return (
     <>
       <SlicemasterGrid>
         {slicemasters.map((master) => (
-          <div key={master.id}>
+          <SlicemasterStyles key={master.id}>
             <Link to={`/slicemaster/${master.slug.current}`}>
               <h2>
-                <span className="mark"> {master.name}</span>
+                <span className="mark">{master.name}</span>
               </h2>
             </Link>
             <Img fluid={master.image.asset.fluid} alt={master.name} />
             <p className="description">{master.description}</p>
-          </div>
+          </SlicemasterStyles>
         ))}
       </SlicemasterGrid>
     </>
