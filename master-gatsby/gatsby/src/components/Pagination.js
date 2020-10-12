c9e9af425d6f8162124cad7c7213eccd7000c729
Paul Tobin <tobster111@gmail.com>
2020-10-12 17:34:49 +0100

    Stying of the pagination

--- master-gatsby/gatsby/src/components/Pagination.js
+++ master-gatsby/gatsby/src/components/Pagination.js
@@ -1,5 +1,31 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import styled from 'styled-components';
+
+const PaginationStyles = styled.div`
+  display: flex;
+  align-content: center;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid var(--grey);
+  margin: 2rem 0;
+  border-radius: 5px;
+  text-align: center;
+  & > * {
+    padding: 1rem;
+    flex: 1;
+    border-right: 1px solid var(--grey);
+    text-decoration: none;
+    &[aria-current],
+    &.current {
+      color: var(--red);
+    }
+    &[disabled] {
+      pointer-events: none;
+      color: var(--grey);
+    }
+  }
+`;
 
 export default function Pagination({
   pageSize,
@@ -15,18 +41,22 @@ export default function Pagination({
   const hasNextPage = nextPage <= totalPages;
   const hasPrevPage = prevPage >= 1;
   return (
-    <div>
+    <PaginationStyles>
       <Link disabled={!hasPrevPage} to={`${base}/${prevPage}`}>
         ⬅ Prev
       </Link>
       {Array.from({ length: totalPages }).map((_, i) => (
-        <Link key={i} to={`${base}/${i > 0 ? i + 1 : ''}`}>
+        <Link
+          className={currentPage === 1 && i === 0 ? 'current' : ''}
+          key={i}
+          to={`${base}/${i > 0 ? i + 1 : ''}`}
+        >
           {i + 1}
         </Link>
       ))}
       <Link disabled={!hasNextPage} to={`${base}/${nextPage}`}>
         Next ➡
       </Link>
-    </div>
+    </PaginationStyles>
   );
 }
