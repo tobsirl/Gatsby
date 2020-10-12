1baed99dbf6752567431cecd700f533945de6a82
Paul Tobin <tobster111@gmail.com>
2020-10-12 17:23:23 +0100

    Display the page numbers to click between

--- master-gatsby/gatsby/src/components/Pagination.js
+++ master-gatsby/gatsby/src/components/Pagination.js
@@ -12,10 +12,21 @@ export default function Pagination({
   const totalPages = Math.ceil(totalCount / pageSize);
   const prevPage = currentPage - 1;
   const nextPage = currentPage + 1;
+  const hasNextPage = nextPage <= totalPages;
+  const hasPrevPage = prevPage >= 1;
   return (
     <div>
-      <Link to={`${base}/${prevPage}`}>⬅ Prev</Link>
-      <Link to={`${base}/${nextPage}`}>Next ➡</Link>
+      <Link disabled={!hasPrevPage} to={`${base}/${prevPage}`}>
+        ⬅ Prev
+      </Link>
+      {Array.from({ length: totalPages }).map((_, i) => (
+        <Link key={i} to={`${base}/${i > 0 ? i + 1 : ''}`}>
+          {i + 1}
+        </Link>
+      ))}
+      <Link disabled={!hasNextPage} to={`${base}/${nextPage}`}>
+        Next ➡
+      </Link>
     </div>
   );
 }
