d34363fab219b4fec28a7a5037fad9b766054ea3
Paul Tobin <tobster111@gmail.com>
2020-10-14 15:32:27 +0100

    Updating the state with the custom hook, value and onChange

--- master-gatsby/gatsby/src/pages/orders.js
+++ master-gatsby/gatsby/src/pages/orders.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import SEO from '../components/SEO';
+import useForm from '../utils/useForm';
 
 export default function Orders() {
+  const { values, updateValue } = useForm({
+    name: '',
+    email: '',
+  });
+
   return (
     <>
       <SEO title="Order a Pizza!" />
@@ -9,9 +15,19 @@ export default function Orders() {
         <fieldset>
           <legend>Your Info</legend>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" />
+          <input
+            type="text"
+            name="name"
+            value={values.name}
+            onChange={updateValue}
+          />
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" />
+          <input
+            type="email"
+            name="email"
+            value={values.email}
+            onChange={updateValue}
+          />
         </fieldset>
         <fieldset>
           <legend>Menu</legend>
