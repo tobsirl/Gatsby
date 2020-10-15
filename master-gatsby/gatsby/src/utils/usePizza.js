db8ea5886ad3f5604efe967170e278f868e3eb72
Paul Tobin <tobster111@gmail.com>
2020-10-15 14:42:10 +0100

    usePizza custom hook

---
+++ master-gatsby/gatsby/src/utils/usePizza.js
@@ -0,0 +1,25 @@
+export default function usePizza({ pizzas, inputs }) {
+  // 1. Create state to hold our order
+  const [order, setOrder] = useState([]);
+  // 2. Make a function add things to order
+  function addToOrder(orderedPizza) {
+    setOrder([...order, orderedPizza]);
+  }
+  // 3. Make a function remove things from order
+  function removeFromOrder(index) {
+    setOrder([
+      // everything before the item we want to remove
+      ...order.slice(0, index),
+
+      // everything after the item we want to remove
+      ...order.slice(index + 1),
+    ]);
+  }
+  // 4. Send this data the a serverless function when they check out
+  // TODO
+  return {
+    order,
+    addToOrder,
+    removeFromOrder,
+  };
+}
