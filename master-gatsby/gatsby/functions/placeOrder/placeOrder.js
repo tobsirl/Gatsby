4de61bb20089c915e0c4bdc00f196c19b4ff11de
Paul Tobin <tobster111@gmail.com>
2020-10-20 16:11:18 +0100

    Validate that the serverless function has the correct data

--- master-gatsby/gatsby/functions/placeOrder/placeOrder.js
+++ master-gatsby/gatsby/functions/placeOrder/placeOrder.js
@@ -11,6 +11,25 @@ const transporter = nodemailer.createTransport({
 });
 
 exports.handler = async (event, context) => {
+  const body = JSON.parse(event.body);
+  console.log(body);
+  // validate the data is correct
+  const requiredFields = ['email', 'name', 'order'];
+  for (const field of requiredFields) {
+    if (!body[field]) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: `Oops! You are missing the ${field} field`,
+        }),
+      };
+    }
+  }
+
+  // send the email
+
+  // send the success or error message
+
   // Test send an email
   const info = await transporter.sendMail({
     from: "Slick's Slices <slick@example.com>",
