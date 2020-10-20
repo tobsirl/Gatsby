3cdac9ba3cf0a1ef8db521ff00d9dbc8bf0c96b2
Paul Tobin <tobster111@gmail.com>
2020-10-20 16:31:29 +0100

    Sending an email with the order

--- master-gatsby/gatsby/functions/placeOrder/placeOrder.js
+++ master-gatsby/gatsby/functions/placeOrder/placeOrder.js
@@ -1,5 +1,20 @@
 const nodemailer = require('nodemailer');
 
+function generateOrderEmail({ order, total }) {
+  return `<div>
+    <h2>Your Recent Order for ${total}</h2>
+    <p>Please start walking over, we will have your order ready in the next 20mins.</p>
+    <ul>
+    ${order.map(
+      (item) => `<li>
+    <img src="${item.thumbnail}" alt="${item.name}"/>
+    ${item.size} ${item.name} - ${item.price}</li>`
+    )}    
+    </ul>
+    <p>Your total is $${total} due at pickup.</p>
+  </div>`;
+}
+
 // create a transport for nodemailer
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
@@ -33,13 +48,13 @@ exports.handler = async (event, context) => {
   // Test send an email
   const info = await transporter.sendMail({
     from: "Slick's Slices <slick@example.com>",
-    to: 'orders@example.com',
+    to: `${body.name} <${body.email}>, orders@example.com`,
     subject: 'New Order!',
-    html: `<p>Your new pizza order is here!</p>`,
+    html: generateOrderEmail({ orde: body.order, total: body.total }),
   });
 
   return {
     statusCode: 200,
-    body: JSON.stringify(info),
+    body: JSON.stringify({ message: 'Success' }),
   };
 };
