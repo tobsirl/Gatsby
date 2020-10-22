59b7ea5bf1662c27fa38eb8859d8fbaae7e8b61d
Paul Tobin <tobster111@gmail.com>
2020-10-22 16:53:06 +0100

    Adding Home Page on the sidebar to update the front page

--- master-gatsby/sanity/sidebar.js
+++ master-gatsby/sanity/sidebar.js
@@ -1,3 +1,20 @@
 import React from 'react';
 import S from '@sanity/desk-tool/structure-builder';
+import storeSettings from './schemas/storeSettings';
 
+// build a custom sidebar
+export default function Sidebar() {
+  return S.list()
+    .title(`Slick's Slices`)
+    .items([
+      // create a new sub item
+      S.listItem()
+        .title('Home Page')
+        .icon(() => <strong>🔥</strong>)
+        .child(S.editor().schemaType('storeSettings').documentId('downtown')),
+
+      ...S.documentTypeListItems().filter(
+        (item) => item.getId() !== 'storeSettings'
+      ),
+    ]);
+}
