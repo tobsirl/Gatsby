a7e258b5b2edb4df79cc49307e03cab5f940e165
Paul Tobin <tobster111@gmail.com>
2020-11-01 17:09:06 +0000

    Setting up the HomePage

--- master-gatsby/gatsby/src/pages/index.js
+++ master-gatsby/gatsby/src/pages/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
+function CurrentlySlicing() {
+  return (
+    <>
+      <p>Test</p>
+    </>
+  );
+}
+
+function HotSlices() {
+  return (
+    <>
+      <p>Test</p>
+    </>
+  );
+}
+
 function HomePage() {
   return (
     <>
-      <p>Hey! I'm the home page</p>
+      <div className="center">
+        <h1>The Best Pizza Downtown!</h1>
+        <p>Open 11am to 11pm Every Single Day</p>
+        <div>
+          <CurrentlySlicing />
+          <HotSlices />
+        </div>
+      </div>
     </>
   );
 }
