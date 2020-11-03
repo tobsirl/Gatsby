9395ad8c91d7fb3b1b2506949e2ae02b608a4399
Paul Tobin <tobster111@gmail.com>
2020-11-03 11:45:17 +0000

    Added component ItemGrid

---
+++ master-gatsby/gatsby/src/components/ItemGrid.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ItemGrid({ items }) {
+  console.log(items);
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  );
+}
--- master-gatsby/gatsby/src/pages/index.js
+++ master-gatsby/gatsby/src/pages/index.js
@@ -2,25 +2,26 @@ import React from 'react';
 import useLatestData from '../utils/useLatestData';
 import { HomePageGrid } from '../styles/Grids';
 import LoadingGrid from '../components/LoadingGrid';
+import ItemGrid from '../components/ItemGrid';
 
 function CurrentlySlicing({ slicemasters }) {
-  console.log(slicemasters);
   return (
     <>
       {!slicemasters && <LoadingGrid count={4} />}
       {slicemasters && !slicemasters?.length && (
         <p>No one is working right now!</p>
       )}
+      {slicemasters?.length && <ItemGrid items={slicemasters} />}
     </>
   );
 }
 
 function HotSlices({ hotSlices }) {
-  console.log(hotSlices);
   return (
     <>
       {!hotSlices && <LoadingGrid count={4} />}
       {hotSlices && !hotSlices?.length && <p>No one is working right now!</p>}
+      {hotSlices?.length && <ItemGrid items={hotSlices} />}
     </>
   );
 }
