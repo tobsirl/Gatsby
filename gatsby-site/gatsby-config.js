382aa94a9eedefd20d0805accaed3037e956ed8e
Paul Tobin <tobster111@gmail.com>
2020-12-08 15:49:45 +0000

    Using the randomuser plugin to fetch data, gatsby-config

--- gatsby-site/gatsby-config.js
+++ gatsby-site/gatsby-config.js
@@ -5,6 +5,12 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: "gatsby-source-randomuser",
+      options: {
+        results: 25,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
