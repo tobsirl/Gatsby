a40937f878a55f8f65fcfb90a37a4a9e88e71312
Paul Tobin <tobster111@gmail.com>
2020-12-10 16:30:48 +0000

    Displaying the flags on the states page

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -31,6 +31,7 @@ const IndexPage = ({ data }) => (
     <h1>Random People</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <Link to={`/page-2`}>States</Link>
     {/* <p>{JSON.stringify(data, null, 2)}</p> */}
     <ul>
       {data.allRandomUser.edges.map(({ node }) => (
--- gatsby-site/src/pages/page-2.js
+++ gatsby-site/src/pages/page-2.js
@@ -1,14 +1,36 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+export const query = graphql`
+  query States {
+    allState {
+      edges {
+        node {
+          name
+          population
+          capital
+          flag
+          id
+        }
+      }
+    }
+  }
+`
+
+const SecondPage = ({ data }) => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    {/* <p>{JSON.stringify(data, null, 2)}</p> */}
+    {data.allState.edges.map(state => (
+      <div>
+        {state.node.name}
+        <img src={state.node.flag} alt={state.name} />
+      </div>
+    ))}
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
