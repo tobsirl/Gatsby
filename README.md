70661b61436676e78a566a441462014468713ca6
Paul Tobin <tobster111@gmail.com>
2020-12-10 15:16:16 +0000

    Example of adding external data

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Gatsby
+# Gatsby [Link](https://www.gatsbyjs.com/)
 Collection of Gatsby projects
+
+## Sourcing data from an external API
+Using external apis in Gatsby using sourceNodes https://www.gatsbyjs.com/docs/recipes/sourcing-data/
+
+```js
+exports.sourceNodes = async params => {
+  // fetch a list of states and source them into gatsby API
+  await Promise.all([fetchStates(params)]) // Add additional apis
+}
+```
--- gatsby-site/gatsby-node.js
+++ gatsby-site/gatsby-node.js
@@ -7,6 +7,18 @@
 // You can delete this file if you're not using it
 const path = require("path")
 
+const fetchStates = async ({ actions, createNodeId, createContentDigest }) => {
+  console.log(`Fetch States into Nodes! ❤`)
+  // 1. Fetch list of states
+  // 2. Loop over each one
+  // 3. Create a node for each state
+}
+
+exports.sourceNodes = async params => {
+  // fetch a list of states and source them into gatsby API
+  await Promise.all([fetchStates(params)])
+}
+
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   const userTemplate = path.resolve(`src/templates/user.js`)
@@ -25,14 +37,13 @@ exports.createPages = async ({ graphql, actions }) => {
     `
   )
 
-  return result.data.allRandomUser.edges.forEach((edge) => {
-    console.log(edge.node.id);
+  return result.data.allRandomUser.edges.forEach(edge => {
     createPage({
       path: `/users/${edge.node.id}`,
       component: path.resolve(userTemplate),
       context: {
         id: edge.node.id,
-      }
+      },
     })
   })
 }
